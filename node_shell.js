361a3803cc799e44c71d20153ebc04eb5b44e16f
doplikef <121702800+doplikef@users.noreply.github.com>
2023-01-03 23:18:12 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'fbe9a31d-92e9-42c5-bc00-c5a4e4a06039'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
